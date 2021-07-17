fda55c0ceb8f97a022c0afb6fb5d82ca50b15d88
Phunbyt <roluwafunbi@gmail.com>
2021-07-17 21:27:27 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,57 @@
+import createError,{HttpError} from'http-errors';
+import express,{Response,Request,NextFunction} from'express';
+import path from'path';
+import cookieParser from'cookie-parser';
+import logger from'morgan';
+import cors from 'cors'
+
+import indexRouter from'./routes/index';
+
+
+var app = express();
+app.use(cors());
+
+// view engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use('/',indexRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+
+
+/* GET home page. */
+router.get("/",  function (req, res, next) {
+  res.send("e dey work");
+});
+
+
+export default router;
